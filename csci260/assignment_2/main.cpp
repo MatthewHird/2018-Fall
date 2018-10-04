dd1426cc8267381f0d6d539fe589b21f764891bd
Matthew Hird <mhird13@gmail.com>
2018-10-03 22:51:00 -0700

    Setup assignment_2

---
+++ csci260/assignment_2/main.cpp
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// @file    main.cpp
+// @author  Matthew Hird
+// @date    October 4, 2018
+//
+// @brief   The main function for the program pex2. Checks commandline
+//          arguments, then starts BatchQueue controller class.
+//------------------------------------------------------------------------------
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    return 0;
+}
