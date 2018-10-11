2a07e88d14b23d4c216a2d826ea553cfe344086a
Matthew Hird <mhird13@gmail.com>
2018-10-10 23:08:07 -0700

    Partially complete  csci265 lab04

---
+++ csci265/lab04/showAccess.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+if [ $# -ne 1 ] ; then
+    echo "Incorrect number of arguments"
+    exit 1
+elif [[ ! -e "$1" ]] ; then
+    echo "$1 does not exist"
+    exit 2
+elif [[ ! -d $1 ]] ; then
+    echo "$1 is not a directory"
+    exit 3
+fi
+
+endInSlash='/$'
+arg=$1
+
+if ! [[ $arg =~ $endInSlash ]] ; then
+    arg="$arg/"
+fi
+
+function dirAccess()
+{
+    if [[ -d $1 ]] ; then
+        if [[ -r $1 ]] && [[ -x $1 ]] ; then
+            echo "    Accessible directory: $1"
+        fi
+
+        for dir in "$1"*/ ; do
+            echo "$dir"
+            dirAccess "$dir"
+        done
+    fi
+
+}
+
+# ls -l "$1"
+
+echo -e "\n*** List of accessible directories within $arg: ***"
+dirAccess "$arg"
+echo -e "*** End of list ***\n"
+
+exit 0
