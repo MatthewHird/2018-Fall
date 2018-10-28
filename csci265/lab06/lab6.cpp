c16e94c8c00a515457c342a2eec260a5d5b44576
Matthew Hird <mhird13@gmail.com>
2018-10-28 11:45:10 -0700

    Complete csci265 lab06

--- csci265/lab06/lab6.cpp
+++ csci265/lab06/lab6.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     // create a vector of lists of integers
-    vector<list<int>> VLI;
+    vector<list<int> > VLI;
 
     // initialize a vector with two lists of integers (1,2,3 and 10,20,30,40)
     insert_list(VLI, { 1, 2, 3 });
--- csci265/lab06/lab6.h
+++ csci265/lab06/lab6.h
@@ -8,17 +8,17 @@ using namespace std;
 
 
 template <typename T>
-void insert_list(vector<list<T>> &VLI, list<T> L)
+void insert_list(vector<list<T> > &VLI, list<T> L)
 {
    VLI.push_back(L);
 }
 
 
 template <typename T>
-void print(vector<list<T>> VLI)
+void print(vector<list<T> > VLI)
 {
     // use iterators to print the contents
-    typename vector<std::list<T>>::iterator V_it;
+    typename vector<std::list<T> >::iterator V_it;
     typename list<T>::iterator L_it;
     for (V_it = VLI.begin(); V_it != VLI.end(); V_it++) {
         list<T> L = (*V_it);
