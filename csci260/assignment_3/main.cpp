b68808b2071e1960ccfa90b75557a4357c7e9056
Matthew Hird <mhird13@gmail.com>
2018-11-18 10:31:06 -0800

    early skeleton

--- csci260/assignment_3/main.cpp
+++ csci260/assignment_3/main.cpp
@@ -1,6 +1,16 @@
+//------------------------------------------------------------------------------
+// @file    main.cpp
+// @author  Matthew Hird
+// @date    November November 18, 2018
+//
+// @brief   The main function for the program pex3. Checks commandline
+//          arguments, then starts ... controller class.
+//------------------------------------------------------------------------------
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int main(int argc, char** argv) {
+
     return 0;
 }
---
+++ csci260/assignment_3/my_exceptions.h
@@ -0,0 +1,87 @@
+//------------------------------------------------------------------------------
+// @file    my_exceptions.h
+// @author  Matthew Hird
+// @date    November 18, 2018
+//
+// @brief   A set of custom exceptions for the program pex3. All exceptions
+//          inherit from std::exception.
+//------------------------------------------------------------------------------
+
+#ifndef ASSIGNMENT_3_MY_EXCEPTIONS_H
+#define ASSIGNMENT_3_MY_EXCEPTIONS_H
+
+#include <exception>
+
+
+class InvalidStudentNumber : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class InvalidStudentName : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class InvalidStreetAddress : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class InvalidCityAddress : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class InvalidEmailAddress : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class DuplicateKeyError: public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class EmptyTreeError: public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class KeyNotFoundError: public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+
+class FullNodeError: public std::exception {
+public:
+    const char* what() const noexcept override {
+        return exception::what();
+    }
+};
+
+#endif //ASSIGNMENT_3_MY_EXCEPTIONS_H
