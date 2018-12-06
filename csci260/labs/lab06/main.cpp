0cf38922a9f4dd1795a632c28a0cd693414ad2eb
Matthew Hird <mhird13@gmail.com>
2018-12-05 23:53:29 -0800

    complete csci260 lab06

--- csci260/labs/lab06/main.cpp
+++ csci260/labs/lab06/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+
+void swap(float* a, float* b) {
+    float temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int partition(float* fArray, int startIndex, int endIndex) {
+    int pivot = fArray[endIndex];
+    int index = startIndex;
+
+    for (int i = startIndex; i < endIndex; ++i) {
+        if (fArray[i] <= pivot) {
+            swap(&fArray[index], &fArray[i]);
+            index++;
+        }
+    }
+    swap(&fArray[index], &fArray[endIndex]);
+    return index;
+}
+
+
+void quicksort(float* fArray, int startIndex, int endIndex) {
+    if (startIndex < endIndex) {
+        int index = partition(fArray, startIndex, endIndex);
+
+        quicksort(fArray, startIndex, index - 1);
+        quicksort(fArray, index + 1, endIndex);
+    }
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    float fArray[] = {3.6, 1, 75, 18.888, 12e5, 1111, 45.335};
+    int size = sizeof(fArray)/sizeof(fArray[0]);
+
+    quicksort(fArray, 0, size - 1);
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << std::to_string(fArray[i]) << "\n";
+    }
+    
     return 0;
 }
