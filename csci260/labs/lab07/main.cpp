3547cb69496289c012b3ab4e4a41b32c761b5155
Matthew Hird <hirdm@stumail.csci.viu.ca>
2018-12-07 00:17:20 -0800

    work on csci260 lav07

--- csci260/labs/lab07/main.cpp
+++ csci260/labs/lab07/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 
+
+int* makeArray(int size) {
+    auto array = new int[size]
+    for (int i = 0; i < size; i++) {
+        array[i] = i + 1;
+    }
+    return array;
+}
+
+
+class Tree {
+public:
+    Tree() {
+        root = nullptr;
+    };
+    ~Tree();
+
+    void insert(int value) {
+        if (!root) {
+            root = new Node(value);
+        }
+
+        
+    };
+private:
+    Node* root;
+};
+
+
+class Node {
+public:
+    explicit Node(int value, Node* left=nullptr, Node* right=nullptr) 
+    : value(value)
+    , balance(0)
+    , left(left)
+    , right(right)
+    {};
+
+    int value;
+    int balance;
+    Node* left;
+    Node* right;
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size = 15;
+    int* myArray = makeArray(size);
+
+
+
+
+
     return 0;
 }
