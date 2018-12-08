54579cd183d8391dea07b27b2fc47cad19df6b0f
Matthew Hird <mhird13@gmail.com>
2018-12-07 17:06:43 -0800

    260 labs

--- csci260/labs/lab10/main.cpp
+++ csci260/labs/lab10/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 
+
+class SkipList {
+public:
+    SkipList() {
+        root = new Cell;
+        root->key = ;
+    };
+
+
+    ~SkipList() {
+
+    };
+
+
+    std::string& lookup(int key) {
+
+    };
+
+
+    bool insert(int key, std::string data) {
+
+    };
+    
+private:
+    struct Cell;
+
+    Cell* search(int key) {
+
+    };
+
+
+    Cell* insertUpRight(Cell* p, Cell*q, int key, std::string* data) {
+
+    };
+
+
+    struct Cell {
+        int key;
+        std::string* data;
+        Cell* up;
+        Cell* down;
+        Cell* left;
+        Cell* right;
+    };
+
+    Cell* root;
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SkipList skipList;
+
     return 0;
 }
